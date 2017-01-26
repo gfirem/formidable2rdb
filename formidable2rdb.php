298677d88552ec2403ea9e45547330970d466a8e
gfirem@gmail.com <gfirem@gmail.com>
2017-01-26 19:55:17 -0400

    Version up

--- formidable2rdb.php
+++ formidable2rdb.php
@@ -1,13 +1,11 @@
 <?php
 /**
- *
- * @since             1.0.1
  * @package           Formidable2Rdb
  *
  * @wordpress-plugin
  * Plugin Name:       Formidable2Rdb
  * Description:       Formidable action to push data to Relational Data Base.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            gfirem
  * License:           Apache License 2.0
  * License URI:       http://www.apache.org/licenses/
