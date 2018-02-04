0f20dd52a74ed7ed99064223450046fb257d6fd9
Guillermo Figueroa Mesa <gfirem@gmail.com>
2018-02-04 15:39:32 -0300

    Updating the name of the plugin for the Formidable required message

--- view/formidable_notice.php
+++ view/formidable_notice.php
@@ -1,7 +1,7 @@
 <div class="error">
 	<p>
 		<?php
-		_e( '<b>Formidable2Rdb</b> requires that Formidable Pro version 2.0 or greater be installed. Until then, keep plugin activated only to continue enjoying this insightful message.', 'formidable2rdb' );
+		_e( '<b>GFireM Mysql Action</b> requires that Formidable Pro version 2.0 or greater be installed. Until then, keep plugin activated only to continue enjoying this insightful message.', 'formidable2rdb' );
 		?>
 	</p>
 </div>
